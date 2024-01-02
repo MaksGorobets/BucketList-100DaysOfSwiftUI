8114e624a926355b8338b902a9902f85793b7a3f
MaksGorobets <gorobets542@gmail.com>
2024-01-02 18:56:08 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Maks Winters on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
